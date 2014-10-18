a53f04dedc9104456715ed1aef6d57102380e909
Alexander Batischev <eual.jp@gmail.com>
2014-10-18 15:41:42 +0300

    Parse BOM

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Data.Attoparsec.Text
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import Control.Monad
+
 main :: IO ()
-main = undefined
+main = do
+  file <- TIO.readFile "./test.bmk.txt"
+  print $ parseOnly pBookmarksFile file
+
+data Bookmark = Bookmark {
+    text    :: T.Text
+  , comment :: T.Text
+  }
+
+instance Show Bookmark where
+  show bmk = T.unpack $ if not . T.null $ comment bmk
+    then T.concat [ text bmk, " (", comment bmk, ")" ]
+    else text bmk
+
+pBookmarksFile :: Parser [Bookmark]
+pBookmarksFile = do
+  pBom
+  return [] -- bmks
+
+pBom :: Parser ()
+pBom = void $ string "\xfeff"
